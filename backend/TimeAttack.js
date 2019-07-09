bd2006086230474406ef38ebede08c0ce166d2d6
Jiro Iwamoto <jirokun@gmail.com>
2019-07-09 22:54:04 +0900

    リファクタリング

--- backend/TimeAttack.js
+++ backend/TimeAttack.js
@@ -1,10 +1,12 @@
 const Game = require('./Game.js');
 const { wait } = require('./util.js');
-const GAME_TIME = 30; // seconds
+const GAME_TIME = 3; // seconds
 
 class TimeAttack extends Game {
+  /** コンストラクタ */
   constructor(io, buttons, leds) {
     super(io, buttons, leds);
+    // stateはコンストラクタで設定する
     this.state = {
       remainSeconds: GAME_TIME,
       score: 0,
@@ -13,6 +15,8 @@ class TimeAttack extends Game {
     };
   }
 
+
+  /** 初期化処理 */
   async init() {
     await wait(1000);
     await this.play('assets/sound/info-girl1-youi2.mp3');
@@ -22,48 +26,52 @@ class TimeAttack extends Game {
     this.getLedPin(this.state.nextIndex).digitalWrite(1);
     this.startTime = new Date().getTime();
     this.seconds = 0;
-    this.timer = setInterval(async () => {
-      const now = new Date().getTime();
-      const seconds = parseInt((now - this.startTime) / 1000);
-      const remainSeconds = GAME_TIME - seconds;
-      if (this.seconds !== seconds) {
-        if (GAME_TIME <= seconds || seconds < 0) {
-          this.clearLeds();
-          this.state.remainSeconds = remainSeconds;
-          this.state.mode = 'SHOW_SCORE';
-          this.stopBgm();
-          this.emitState();
-          clearInterval(this.timer);
-          await this.play('assets/sound/info-girl1-zero1.mp3');
-          await this.play('assets/sound/info-girl1-sokomade1.mp3');
-          await wait(500);
-          await this.play('assets/sound/jingle-jazz.mp3');
-          this.playBgm('assets/bgm/何を作っているのかな？_volume.mp3');
-          await this.waitForAnyButtonPushed();
-          this.end();
-          return;
-        }
-        if (remainSeconds === 10) this.play('assets/sound/info-girl1-zyuu1.mp3');
-        if (remainSeconds === 9) this.play('assets/sound/info-girl1-kyuu1.mp3');
-        if (remainSeconds === 8) this.play('assets/sound/info-girl1-hachi1.mp3');
-        if (remainSeconds === 7) this.play('assets/sound/info-girl1-nana1.mp3');
-        if (remainSeconds === 6) this.play('assets/sound/info-girl1-roku1.mp3');
-        if (remainSeconds === 5) this.play('assets/sound/info-girl1-go1.mp3');
-        if (remainSeconds === 4) this.play('assets/sound/info-girl1-yon1.mp3');
-        if (remainSeconds === 3) this.play('assets/sound/info-girl1-san1.mp3');
-        if (remainSeconds === 2) this.play('assets/sound/info-girl1-ni1.mp3');
-        if (remainSeconds === 1) this.play('assets/sound/info-girl1-ichi1.mp3');
-        this.seconds = seconds;
-        this.state.remainSeconds = GAME_TIME - seconds;
-        this.emitState();
-      }
-    }, 100);
+    this.timer = setInterval(this.loop.bind(this), 100);
   }
+  /** 終了時処理 */
   destroy() {
     super.destroy();
     clearInterval(this.timer);
   }
+  /** 100ms毎に呼ばれる */
+  async loop() {
+    const now = new Date().getTime();
+    const seconds = parseInt((now - this.startTime) / 1000);
+    const remainSeconds = GAME_TIME - seconds;
+    if (this.seconds !== seconds) {
+      if (GAME_TIME <= seconds || seconds < 0) {
+        this.clearLeds();
+        this.state.remainSeconds = remainSeconds;
+        this.state.mode = 'SHOW_SCORE';
+        this.stopBgm();
+        this.emitState();
+        clearInterval(this.timer);
+        await this.play('assets/sound/info-girl1-zero1.mp3');
+        await this.play('assets/sound/info-girl1-sokomade1.mp3');
+        await wait(500);
+        await this.play('assets/sound/jingle-jazz.mp3');
+        this.playBgm('assets/bgm/何を作っているのかな？_volume.mp3');
+        await this.waitForAnyButtonPushed();
+        this.end();
+        return;
+      }
+      if (remainSeconds === 10) this.play('assets/sound/info-girl1-zyuu1.mp3');
+      if (remainSeconds === 9) this.play('assets/sound/info-girl1-kyuu1.mp3');
+      if (remainSeconds === 8) this.play('assets/sound/info-girl1-hachi1.mp3');
+      if (remainSeconds === 7) this.play('assets/sound/info-girl1-nana1.mp3');
+      if (remainSeconds === 6) this.play('assets/sound/info-girl1-roku1.mp3');
+      if (remainSeconds === 5) this.play('assets/sound/info-girl1-go1.mp3');
+      if (remainSeconds === 4) this.play('assets/sound/info-girl1-yon1.mp3');
+      if (remainSeconds === 3) this.play('assets/sound/info-girl1-san1.mp3');
+      if (remainSeconds === 2) this.play('assets/sound/info-girl1-ni1.mp3');
+      if (remainSeconds === 1) this.play('assets/sound/info-girl1-ichi1.mp3');
+      this.seconds = seconds;
+      this.state.remainSeconds = GAME_TIME - seconds;
+      this.emitState();
+    }
+  }
 
+  /** ボタンが押されたときの処理 */
   onPushed(pin) {
     if (this.state.mode !== 'PLAYING') return;
     if (pin !== this.state.nextIndex) {
@@ -78,6 +86,7 @@ class TimeAttack extends Game {
     this.emitState();
   }
 
+  /** 次のボタンのインデックスを取得する。同じインデックスは採番しない */
   getNextIndex() {
     while (true) {
       const nextIndex = Math.floor(Math.random() * 10);
